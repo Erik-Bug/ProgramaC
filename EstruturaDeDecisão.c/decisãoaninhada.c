694930a31ef42584c4c855b2be7176137438e79c
Erik-Bug <erikbarboza07@gmail.com>
2025-09-19 16:30:07 +0000

    a

---
+++ EstruturaDeDecisão.c/decisãoaninhada.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    // idade deve ser maior que 18 e menor que 65
+    // renda menor 3000
+    // dependencias devem ser maior que 2
+
+    int idade;
+    int renda;
+    int dependecnia;
+
+    printf("Digite sua idade: \n");
+    scanf("%d", &idade);
+    printf("Digite sua renda mensal: \n");
+    scanf("%d", &renda);
+    printf("Digite a quantidade de dependendcias: \n");
+    scanf("%d")
+
+
+    return 0;
+}
