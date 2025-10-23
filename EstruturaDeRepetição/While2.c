c488cd9d2c9155d0f39db8a621477946697a4e7c
Erik-Bug <erikbarboza07@gmail.com>
2025-10-23 17:44:31 +0000

    EstruturaDeRepeti

---
+++ EstruturaDeRepetição/While2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+    int num;
+
+    printf("Digite um número (negativo para sair) ");
+    scanf("%d", &num);
+
+    while(num >= 0){
+        printf("Você digitou: %d\n", num);
+
+        printf("Digite um Número (negativo para sair): ");
+        scanf("%d", &num);
+    }
+    
+    return 0;
+}
